df5e417d94ec8bd536477f55dc874467c9e2c9c7
王志军 <hw_developer@163.com>
2018-08-12 18:02:25 +0800

    更新数据

---
+++ 04/01/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: []
+    }
+};
